e5d444c09060c73d50be6578d1bf74aace82249e
HassanThePassionate <maqsoodia777@gmail.com>
2025-02-23 20:13:51 +0500

    Upate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Options from "./pages/Options";
-import ScreenshotHistory from "./pages/screenShotHistory";
+import ScreenshotHistory from "./pages/screenshotHistory";
 
 function App() {
   return (
